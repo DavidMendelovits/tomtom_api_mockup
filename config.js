1d8b7596ec85b4abcae537b9736954173c7b0690
David Mendelovits <david.mendelovits@gmail.com>
2019-02-24 12:42:17 -0800

    stuff

---
+++ config.js
@@ -0,0 +1,3 @@
+var apiKey = 'cqGnYDa6AgNt6NNQbNaeZiGiz4w86NPy';
+var geofencingAdminKey = 'oOWZG098GnaDhxCxXc0pWOsPxU9wCfqBFByvGimgRKDe42Xx';
+var geofencingProjectId = '17e386dc-938b-4f5e-b602-24fc998904e8';
